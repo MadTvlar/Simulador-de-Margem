5e5f95eab25d395d8b4a906cf0349367d37478dc
Madson1999 <madson_michel@hotmail.com>
2025-03-22 15:58:53 -0400

    mt 03 24-25

--- dados/dados_motos.py
+++ dados/dados_motos.py
@@ -312,6 +312,19 @@ motos = { #ULTIMA ALTERAÇÃO: __/__/__
         'revisao': 0,
     },
 
+    'MT-03 ABS 24-25': { #ULTIMA ALTERAÇÃO: 07/02/2025
+        # MANAUS
+        'manaus_custo_produto': 29503.77,
+        'manaus_pps': 0,
+
+        # INTERIOR
+        'interior_custo_produto': 29503.77,
+        'interior_pps': 0,
+
+        # GERAL
+        'revisao': 0,
+    },
+
      'MT-07 ABS 24-25': { #ULTIMA ALTERAÇÃO: 07/02/2025
         # MANAUS
         'manaus_custo_produto': 42250.26,
